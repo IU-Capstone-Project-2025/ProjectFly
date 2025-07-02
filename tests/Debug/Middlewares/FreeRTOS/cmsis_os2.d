10f93951674956d51754b3a35ea94ec7237dded1
Demetrius <thedioxider@mail.ru>
2025-07-02 23:52:43 +0300

    test: work test

---
+++ tests/Debug/Middlewares/FreeRTOS/cmsis_os2.d
@@ -0,0 +1,36 @@
+Middlewares/FreeRTOS/cmsis_os2.o: \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h \
+ C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/portmacro.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/event_groups.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/timers.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/semphr.h:
+C:/Users/Dioxider/STM32Cube/Repository/STM32Cube_FW_F7_V1.17.3/Middlewares/Third_Party/FreeRTOS/Source/include/queue.h:
